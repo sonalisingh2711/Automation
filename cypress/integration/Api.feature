2217b60a001e684a489d22050829347430ef2cc7
sonalisingh2711 <singhsonali095@gmail.com>
2021-06-03 15:33:49 +0530

    Api automation

--- cypress/integration/Api.feature
+++ cypress/integration/Api.feature
@@ -1,34 +1,17 @@
 Feature: testing Api functionality
 
     Scenario: get request
-        Given "GET" request for "/2" Endpoint
-        When check id of body
-        Then "@users" verify status code 200
-
-    Scenario: delete request
-        Given "DELETE" request for "/2" Endpoint
-        Then "@users" verify status code 204
-        Then User must be empty
-
-    Scenario: Post request
-        Given "POST" request for "/users/2" Endpoint and pass data of index 0
-        Then "@body" verify status code 201
-
-    Scenario: Put request
-        Given "PUT" request for "/users/2" Endpoint and pass data of index 1
-
-    Scenario: Post request
-        Given "POST" request for "/login" Endpoint and pass data of index 2
-        Then  check for token generated
-        
-    Scenario: Post request
-        Given "POST" request for "/login" Endpoint and pass data of index 3
-        Then "@body" verify status code 200
-
-
-
-
-
-
-
-
+        Given I request for get/ users endpoint
+        When I check status of get/ to be equal to 200
+    Scenario: get request
+        Given I request for users endpoint not found
+        When I check status of get/ to be equal to 404
+    Scenario: create user
+        Given I request for post/ users endpoint
+        When I check status of post/ to be equal to 201
+    Scenario: login user
+        Given I request for post/ users endpoint for login with valid credentials
+        When I check status of post/ to be equal to 201
+     Scenario: login user with invalid credentials
+        Given I request for post/ users endpoint for login with invalid credentials
+        When I check status of post/ to be equal to 400
--- cypress/integration/Api_stepdefinition/Api.js
+++ cypress/integration/Api_stepdefinition/Api.js
@@ -1,27 +1,26 @@
 import { expect } from 'chai'
 import { Given, When, Then, And } from 'cypress-cucumber-preprocessor/steps'
 import data from '../../fixtures/ApiData.json'
-Given('{string} request for {string} Endpoint', (request,endpoint,) => {
-    cy.requestWithoutBody(request,endpoint).as('users')
-});
-Then ('{string} verify status code {int}',(alias,status)=>{
-cy.get(alias).its('status').should("be.eq",status)
+import Url from '../../fixtures/UrlAndEndpoint.json'
+Given('I request for get/ users endpoint', () => {
+    cy.getRequest(Url.baseUrl + Url.getuser)
 })
-Then("User must be empty",()=>{
-    cy.get('@users').then((response)=>{
-        expect(response.body).to.be.empty
-    });
+When('I check status of get/ to be equal to {int}', (status) => {
+    cy.checkStatusCodeForGet(status)
 })
-When('check id of body',()=>{
-    cy.get('@users').then((response)=>{
-        expect(response.body.data.id).be.eq(2)
-       })
+Given('I request for users endpoint not found', () => {
+    cy.getRequest(Url.baseUrl + Url.getuser23)
 })
-Given("{string} request for {string} Endpoint and pass data of index {int}",(request,endpoint,index)=>{
-
-    cy.requestWithBody(request,endpoint,data[index]).as('body')
+Given('I request for post/ users endpoint', () => {
+    cy.postRequest(Url.baseUrl + Url.getuser, data[0])
 })
-Then("check for token generated",()=>{
-    cy.get('@body').its('body').should('have.property','token')
+When('I check status of post/ to be equal to {int}', (status) => {
+    cy.checkStatusCodeForPost(status)
+})
+Given('I request for post/ users endpoint for login with valid credentials', () => {
+    cy.postRequest(Url.baseUrl + Url.login, data[2])
+})
+Given('I request for post/ users endpoint for login with invalid credentials', () => {
+    cy.postRequest(Url.baseUrl + Url.login, data[3])
 })
 
